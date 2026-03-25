572d6c58fa4c639c30d44d0ad06c643a50ca0e79
William Elesbão <w.e@deggex.awsapps.com>
2026-03-25 20:18:19 -0300

    chore: consolidate query client setup into a single file and remove redundant code

--- apps/web/src/lib/react-query/index.ts
+++ apps/web/src/lib/react-query/index.ts
@@ -1,3 +1,22 @@
-import { QueryClient } from "@tanstack/react-query";
+import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onError: (error) => {
+      console.error(error.name);
+      console.error(error.message);
+    },
+  }),
+  queryCache: new QueryCache({
+    onError: (error) => {
+      console.error(error.name);
+      console.error(error.message);
+    },
+  }),
+  defaultOptions: {
+    queries: {
+      refetchOnReconnect: true,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
