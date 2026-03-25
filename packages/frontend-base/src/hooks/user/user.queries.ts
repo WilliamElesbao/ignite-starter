6aa579626208a0e3fa8441c832ffcfd2327e298e
William Elesbão <w.e@deggex.awsapps.com>
2026-03-25 19:24:05 -0300

    chore: remove unused user queries and demo page components

--- packages/frontend-base/src/hooks/user/user.queries.ts
+++
@@ -1,7 +0,0 @@
-import { getUserByIdOptions } from "@repo/api";
-import { useQuery } from "@tanstack/react-query";
-
-export const useGetUserById = () =>
-  useQuery({
-    ...getUserByIdOptions({ path: { id: "wcUjRNf4TvH5NdnabEnzHoIddTsdLWxO" } }),
-  });
