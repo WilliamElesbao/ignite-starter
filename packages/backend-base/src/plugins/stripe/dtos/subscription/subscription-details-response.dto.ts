14e0a391cfdb970accc015c4adcfc6ec31b2f5f9
William Elesbão <w.e@deggex.awsapps.com>
2026-03-28 16:58:34 -0300

    refactor: remove unused JSON example and clean up code formatting in SubscriptionDetailsResponseDto

--- packages/backend-base/src/plugins/stripe/dtos/subscription/subscription-details-response.dto.ts
+++ packages/backend-base/src/plugins/stripe/dtos/subscription/subscription-details-response.dto.ts
@@ -1,27 +1,5 @@
 import { t } from "elysia";
 
-/**
- * {
-    "id": "sub_1TFzX6Dj31fYLKFP3KPsqmWb",
-    "status": "active",
-    "current_period_start": "2026-03-28T16:30:59.000Z",
-    "current_period_end": "2026-09-28T16:30:59.000Z",
-    "cancel_at_period_end": false,
-    "customer": "cus_UESRmQ45e77dJO",
-    "created": "2026-03-28T16:16:18.000Z",
-    "plan": {
-        "priceId": "price_1TEFrJDj31fYLKFPz0F5MIcp",
-        "amount": 2500,
-        "currency": "usd",
-        "interval": "month"
-    },
-    "product": {
-        "id": "prod_UCfBp9mZb5TWCT",
-        "name": "Premium",
-        "description": "Best Seller"
-    }
-}
- */
 const STRIPE_SUBSCRIPTION_STATUS = [
   "incomplete",
   "incomplete_expired",
@@ -54,17 +32,21 @@ const SubscriptionDetailsDataDto = t.Object({
   cancel_at_period_end: t.Optional(t.Boolean()),
   customer: t.Optional(StripeCustomer),
   created: t.Optional(t.String()),
-  plan: t.Optional(t.Object({
-    priceId: t.String(),
-    amount: t.Nullable(t.Number()),
-    currency: t.String(),
-    interval: t.Optional(StripeIntervalEnum),
-  })),
-  product: t.Optional(t.Object({
-    id: t.String(),
-    name: t.String(),
-    description: t.Nullable(t.String()),
-  })),
+  plan: t.Optional(
+    t.Object({
+      priceId: t.String(),
+      amount: t.Nullable(t.Number()),
+      currency: t.String(),
+      interval: t.Optional(StripeIntervalEnum),
+    }),
+  ),
+  product: t.Optional(
+    t.Object({
+      id: t.String(),
+      name: t.String(),
+      description: t.Nullable(t.String()),
+    }),
+  ),
 });
 
 export const SubscriptionDetailsResponseDto = SubscriptionDetailsDataDto;
