ded0f082094299f1c68a623c568381d58e9c1ed8
William Elesbão <w.e@deggex.awsapps.com>
2026-03-29 21:17:15 -0300

    refactor: simplify useSendEmail hook implementation

--- apps/web/src/hooks/email/email.mutation.ts
+++ apps/web/src/hooks/email/email.mutation.ts
@@ -1,7 +1,4 @@
 import { postEmailSendMutation } from "@repo/api/";
 import { useMutation } from "@tanstack/react-query";
 
-export const useSendEmail = () =>
-  useMutation({
-    ...postEmailSendMutation(),
-  });
+export const useSendEmail = () => useMutation({ ...postEmailSendMutation() });
