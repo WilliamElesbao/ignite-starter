7028c2c9e473cc6191501094c8f6cb01d1f5dcfc
William Elesbão <w.e@deggex.awsapps.com>
2026-03-29 21:18:08 -0300

    refactor: restore mutation and query cache configurations in queryClient

--- apps/web/src/lib/react-query/index.ts
+++ apps/web/src/lib/react-query/index.ts
@@ -1,25 +1,22 @@
-import {
-  //  MutationCache, QueryCache,
-  QueryClient,
-} from "@tanstack/react-query";
+import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient({
-  // mutationCache: new MutationCache({
-  //   onError: (error) => {
-  //     console.error(error.name);
-  //     console.error(error.message);
-  //   },
-  // }),
-  // queryCache: new QueryCache({
-  //   onError: (error) => {
-  //     console.error(error.name);
-  //     console.error(error.message);
-  //   },
-  // }),
-  // defaultOptions: {
-  //   queries: {
-  //     refetchOnReconnect: true,
-  //     refetchOnWindowFocus: false,
-  //   },
-  // },
+  mutationCache: new MutationCache({
+    onError: (error) => {
+      console.error(error.name);
+      console.error(error.message);
+    },
+  }),
+  queryCache: new QueryCache({
+    onError: (error) => {
+      console.error(error.name);
+      console.error(error.message);
+    },
+  }),
+  defaultOptions: {
+    queries: {
+      refetchOnReconnect: true,
+      refetchOnWindowFocus: false,
+    },
+  },
 });
