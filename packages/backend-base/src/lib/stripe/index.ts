c8362eaf86256c27c9a626f6f66236fcadfa6600
William Elesbao <william.elesbao.2000@gmail.com>
2026-07-13 13:08:43 -0300

    refactor: update Stripe API version to 2026-06-24.dahlia

--- packages/backend-base/src/lib/stripe/index.ts
+++ packages/backend-base/src/lib/stripe/index.ts
@@ -2,5 +2,5 @@ import Stripe from "stripe";
 import { env } from "../../env";
 
 export const stripe = new Stripe(env.STRIPE_API_KEY, {
-  apiVersion: "2026-02-25.clover",
+  apiVersion: "2026-06-24.dahlia",
 });
