52c75e3cacecdb4e0525d820673efea4f143d75d
William Elesbao <william.elesbao.2000@gmail.com>
2026-07-16 17:46:32 -0300

    refactor(subscription): remove unused subscription schema file

--- apps/web/src/features/subscription/schema/use-subscription-schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-const subscriptionSchema = z.object({
-  priceId: z.string(),
-  planName: z.string(),
-  subscriptionId: z.string().optional(),
-});
-
-export type SubscriptionSchema = z.infer<typeof subscriptionSchema>;
